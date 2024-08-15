628062645bc063f14724be6e6ce95cdcb5423fce
Adam Liga <adam.liga@student.tuke.sk>
2024-08-16 00:45:47 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: theme => ({
-        'pexels-19x14-8478248' : "url('../img/pexels-19x14-8478248.jpg')"
-      })
-    },
+    extend: {},
   },
   plugins: [],
 }
